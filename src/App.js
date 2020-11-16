1ba0f7fa9bf1c1f43fa4bd65f32658ff9a3a2313
Saahil Shaikh <saahilshaikhrocks@gmail.com>
2020-11-16 18:41:31 +0530

    11162020

--- src/App.js
+++ src/App.js
@@ -22,44 +22,32 @@ import Loader from "./Layout/Components/Loader/Loader";
 import Sale from "./Layout/Pages/Sale/Sale";
 
 function App() {
-  const location = useLocation();
-  return (
-    <div className="App">
-      <Couponbar />
-      <Navbar />
-      <MiniNav />
-      <AnimatePresence>
-        <Switch location={location} key={location.pathname}>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Category/:id" component={CategoryList} />
-          <Route exact path="/Category/:id1/:id2" component={ProductList} />
-          <Route
-            exact
-            path="/Category/:id1/:id2/:id3"
-            component={ProductDesc}
-          />
-          <Route exact path="/Dashboard/:id" component={Dashboard} />
-          <Route exact path="/ComingSoon" component={ComingSoon} />
-          <Route exact path="/NewArrivals" component={NewArrival} />
-          <Route
-            exact
-            path="/Cart/Checkout/coupon::coupon"
-            component={Checkout}
-          />
-          <Route
-            exact
-            path="/Checkout/:item/:quantity"
-            component={ByeNow}
-          />
-          <Route exact path="/Orders/:id" component={Order} />
-          <Route exact path="/Sale" component={Sale} />
-          <Route exact path="/Products/:id" component={Viewall} />
-          <Route exact path="*" component={NotFound} />
-        </Switch>
-      </AnimatePresence>
-      <Footer />
-    </div>
-  );
+	const location = useLocation();
+	return (
+		<div className='App'>
+			<Couponbar />
+			<Navbar />
+			<MiniNav />
+			<AnimatePresence>
+				<Switch location={location} key={location.pathname}>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/Category/:id' component={CategoryList} />
+					<Route exact path='/Category/:id1/:id2' component={ProductList} />
+					<Route exact path='/Category/:id1/:id2/:id3' component={ProductDesc} />
+					<Route exact path='/Dashboard/:id' component={Dashboard} />
+					<Route exact path='/ComingSoon' component={ComingSoon} />
+					<Route exact path='/NewArrivals' component={NewArrival} />
+					<Route exact path='/Cart/Checkout/coupon::coupon' component={Checkout} />
+					<Route exact path='/Checkout/:item/:quantity' component={ByeNow} />
+					<Route exact path='/Orders/:id' component={Order} />
+					<Route exact path='/Sale' component={Sale} />
+					<Route exact path='/Products/:id' component={Viewall} />
+					<Route exact path='*' component={NotFound} />
+				</Switch>
+			</AnimatePresence>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
