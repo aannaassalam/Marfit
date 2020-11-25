f1f2f44cd959e56b4e071c311d0625286f463cd1
Saahil Shaikh <saahilshaikhrocks@gmail.com>
2020-11-25 13:51:10 +0530

    11252020

--- src/Layout/Components/Footer/Footer.js
+++ src/Layout/Components/Footer/Footer.js
@@ -5,73 +5,71 @@ import cards from "../../../assets/cards.png";
 import email from "../../../assets/email.png";
 
 export default class Footer extends React.Component {
-  render() {
-    return (
-      <div className="footer">
-        <div className="flex-container">
-          <div className="container-1">
-            <div className="marfit-detail">
-              <img src={marfitLogo} alt="Marfit Logo" />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation .
-              </p>
-            </div>
-            <div className="company-terms">
-              <div className="company-items">
-                <h3>COMPANY</h3>
-                <a href="#">Track Orders</a>
-                <a href="#">About Us</a>
-                <a href="#">Contact</a>
-              </div>
+	render() {
+		return (
+			<div className='footer'>
+				<div className='flex-container'>
+					<div className='container-1'>
+						<div className='marfit-detail'>
+							<img src={marfitLogo} alt='Marfit Logo' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. sed do eiusmod tempor incididunt ut labore et dolore magna
+								aliqua. Ut enim ad minim veniam, quis nostrud exercitation .
+							</p>
+						</div>
+						<div className='company-terms'>
+							<div className='company-items'>
+								<h3>COMPANY</h3>
+								<a href='#'>Track Orders</a>
+								<a href='#'>About Us</a>
+								<a href='#'>Contact</a>
+							</div>
 
-              <div className="company-items">
-                <h3>POLICY & RULES</h3>
-                <a href="#">Returns & Refunds</a>
-                <a href="#">Privacy Policy</a>
-                <a href="#">Terms & Condition</a>
-              </div>
+							<div className='company-items'>
+								<h3>POLICY & RULES</h3>
+								<a href='#'>Returns & Refunds</a>
+								<a href='#'>Privacy Policy</a>
+								<a href='#'>Terms & Condition</a>
+							</div>
 
-              <div className="company-items">
-                <h3>SUPPORT</h3>
-                <p>Chat with us</p>
-                <a href="tel:1234567890">
-                  <i className="fas fa-phone-alt"></i>+91 XXX XXX XXXX
-                </a>
-                <a href="mailto:info@marfit.com">
-                  <i className="fas fa-envelope"></i> info@marfit.com
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="container-2">
-            <p className="label">IT FEELS GREAT TO KNOW</p>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter email address *"
-            />
-            <div className="icons-pack">
-              <a href="#">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram"></i>
-              </a>
-            </div>
-            <p>We Accept</p>
-            <img src={cards} alt="CardsImage" />
-          </div>
-        </div>
-        <div className="copyright">
-          <p>&copy; Copyright 2020 . All rights reserved</p>
-        </div>
-      </div>
-    );
-  }
+							<div className='company-items'>
+								<h3>SUPPORT</h3>
+								<a href='tel:+919007589067'>
+									<i className='fas fa-phone-alt'></i>+91 9007 589 067
+								</a>
+								<a href='mailto:info@marfit.com'>
+									<i className='fas fa-envelope'></i> info@marfit.com
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className='container-2'>
+						<p className='label'>IT FEELS GREAT TO KNOW</p>
+						<input type='email' name='email' id='email' placeholder='Enter email address *' />
+						<div className='icons-pack'>
+							<a href='#'>
+								<i className='fab fa-facebook-f'></i>
+							</a>
+							<a href='#'>
+								<i className='fab fa-twitter'></i>
+							</a>
+							<a href='#'>
+								<i className='fab fa-instagram'></i>
+							</a>
+						</div>
+						<p>We Accept</p>
+						<img src={cards} alt='CardsImage' />
+					</div>
+				</div>
+				<div className='copyright'>
+					<p>
+						&copy; Copyright 2020 . All rights reserved | Powered by{" "}
+						<a style={{ color: "#FC864D" }} href='https://www.augendtech.com' target='_blank'>
+							Augend Tech
+						</a>
+					</p>
+				</div>
+			</div>
+		);
+	}
 }
--- src/Layout/Components/MiniNav/MiniNav.js
+++ src/Layout/Components/MiniNav/MiniNav.js
@@ -1,63 +1,64 @@
 import React from "react";
 import "./MiniNav.css";
 import Lottie from "lottie-react-web";
-import loading  from "../../../assets/loading.json";
+import loading from "../../../assets/loading.json";
 import firebase from "firebase";
 import Loader from "../Loader/Loader";
 
 export default class MiniNav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [],
-      loading: true,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			categories: [],
+			loading: true,
+		};
+	}
 
-  componentDidMount() {
-    firebase
-      .firestore()
-      .collection("settings")
-      .onSnapshot((snap) => {
-        snap.docChanges().forEach((change) => {
-          this.setState({
-            categories: change.doc.data().categories,
-            loading: false,
-          });
-        });
-      });
-  }
-  render() {
-    return (
-      <div className="mini-container">
-          {this.state.loading ? (
-            <Loader />
-          ) : (
-            <>
-            <a href="/NewArrivals" className="new">New Arrivals</a>
-              {this.state.categories.map((cat, index) => {
-                return (
-                  <div className="mini-content" key={index}>
-                    {cat.name} <i className="fa fa-chevron-down fa-1x"></i>
-                    <div className="category-options">
-                      {cat.subcategories.map((sub, index) => {
-                        return (
-                          <a
-                            href={"/Category/" + cat.name + "/" + sub.name}
-                            key={index}
-                          >
-                            {sub.name}
-                          </a>
-                        );
-                      })}
-                    </div>
-                  </div>
-                );
-              })}
-              <a href="/Sale" className="sale">Sale</a>
-            </>
-          )}
-      </div>
-    );
-  }
+	componentDidMount() {
+		firebase
+			.firestore()
+			.collection("settings")
+			.onSnapshot((snap) => {
+				snap.docChanges().forEach((change) => {
+					this.setState({
+						categories: change.doc.data().categories,
+						loading: false,
+					});
+				});
+			});
+	}
+	render() {
+		return (
+			<div className='mini-container'>
+				{this.state.loading ? (
+					<Loader />
+				) : (
+					<>
+						<a href='/NewArrivals' className='new'>
+							New Arrivals
+						</a>
+						{this.state.categories.map((cat, index) => {
+							return (
+								<a href={"/Category/" + cat.name} className='mini-content' key={index}>
+									{cat.name} <i className='fa fa-chevron-down fa-1x'></i>
+									<div className='category-options'>
+										{cat.subcategories.map((sub, index) => {
+											return (
+												<a href={"/Category/" + cat.name + "/" + sub.name} key={index}>
+													{sub.name}
+												</a>
+											);
+										})}
+									</div>
+								</a>
+							);
+						})}
+						<a href='/Sale' className='sale'>
+							Sale
+						</a>
+					</>
+				)}
+			</div>
+		);
+	}
 }
